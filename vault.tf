2b55a16258c7c412822661c7bff98c71ec08aa69
dorinahashicorp <dorina.timbur@hashicorp.com>
2025-12-03 10:33:44 +0000

    fix: use vault_kv_secret_v2 data source instead of deprecated vault_generic_secret

--- vault.tf
+++ vault.tf
@@ -16,15 +16,16 @@ provider "vault" {
   }
 }
 
-# Retrieve Vault secrets from HCP Vault KV (v2 format requires /data/)
-data "vault_generic_secret" "vault_secrets" {
-  path = "${var.vault_kv_mount}/data/${var.vault_kv_path_prefix}"
+# Retrieve Vault secrets from HCP Vault KV v2
+data "vault_kv_secret_v2" "vault_secrets" {
+  mount = var.vault_kv_mount
+  name  = var.vault_kv_path_prefix
 }
 
 # Extract individual secrets for use in the module
 locals {
-  vault_license     = data.vault_generic_secret.vault_secrets.data["license"]
-  vault_server_cert = data.vault_generic_secret.vault_secrets.data["server_cert"]
-  vault_server_key  = data.vault_generic_secret.vault_secrets.data["server_key"]
-  vault_ca_cert     = data.vault_generic_secret.vault_secrets.data["ca_cert"]
+  vault_license     = data.vault_kv_secret_v2.vault_secrets.data["license"]
+  vault_server_cert = data.vault_kv_secret_v2.vault_secrets.data["server_cert"]
+  vault_server_key  = data.vault_kv_secret_v2.vault_secrets.data["server_key"]
+  vault_ca_cert     = data.vault_kv_secret_v2.vault_secrets.data["ca_cert"]
 }
